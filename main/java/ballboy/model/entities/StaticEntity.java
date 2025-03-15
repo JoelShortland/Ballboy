cf4dac1b91cdf46ce10fddd793eecfcd1642bd8e
JoelShortland <43557662+JoelShortland@users.noreply.github.com>
2025-03-15 17:20:20 +1100

    Delete main directory

--- main/java/ballboy/model/entities/StaticEntity.java
+++
@@ -1,9 +0,0 @@
-package ballboy.model.entities;
-
-import ballboy.model.Entity;
-
-/**
- * A static entity instance depended on by Levels.
- */
-public abstract class StaticEntity implements Entity {
-}
